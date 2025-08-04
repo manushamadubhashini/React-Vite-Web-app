18d620f5ab1871380535e14da401f30455b42d45
Acer <madubhashinimanusha@gmail.com>
2025-08-04 13:31:26 +0530

    Add DefaultLayout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import './App.css'
-import {Navbar} from "./view/common/Navbar/Navbar.tsx";
-import {MainContent} from "./view/common/MainContent/MainContent.tsx";
-import {Footer} from "./view/common/Footer/Footer.tsx";
 import {BrowserRouter} from "react-router-dom";
+import {DefaultLayout} from "./view/common/DefaultLayout/DefaultLayout.tsx";
 
 function App() {
 
   return (
     <div className="app">
         <BrowserRouter>
-     <Navbar/>
-        <MainContent/>
-        <Footer/>
+            <DefaultLayout/>
+
         </BrowserRouter>
 
     </div>
